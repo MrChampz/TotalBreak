87223eadd87321ee3e744abce21cb8b8d8661037
Felipe <mrchampz@outlook.com>
2016-12-21 03:03:50 -0200

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/TotalBreak.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/mrchampz/ClionProjects/TotalBreak/Source/Game.cpp" "/home/mrchampz/ClionProjects/TotalBreak/cmake-build-debug/CMakeFiles/TotalBreak.dir/Source/Game.cpp.o"
+  "/home/mrchampz/ClionProjects/TotalBreak/Source/Program.cpp" "/home/mrchampz/ClionProjects/TotalBreak/cmake-build-debug/CMakeFiles/TotalBreak.dir/Source/Program.cpp.o"
+  "/home/mrchampz/ClionProjects/TotalBreak/Source/ResourceManager.cpp" "/home/mrchampz/ClionProjects/TotalBreak/cmake-build-debug/CMakeFiles/TotalBreak.dir/Source/ResourceManager.cpp.o"
+  "/home/mrchampz/ClionProjects/TotalBreak/Source/Shader.cpp" "/home/mrchampz/ClionProjects/TotalBreak/cmake-build-debug/CMakeFiles/TotalBreak.dir/Source/Shader.cpp.o"
+  "/home/mrchampz/ClionProjects/TotalBreak/Source/Texture.cpp" "/home/mrchampz/ClionProjects/TotalBreak/cmake-build-debug/CMakeFiles/TotalBreak.dir/Source/Texture.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLFW_DLL"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../Header"
+  "../Dependencies/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/TotalBreak.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TotalBreak.dir/Source/Program.cpp.o"
+  "CMakeFiles/TotalBreak.dir/Source/Game.cpp.o"
+  "CMakeFiles/TotalBreak.dir/Source/Shader.cpp.o"
+  "CMakeFiles/TotalBreak.dir/Source/Texture.cpp.o"
+  "CMakeFiles/TotalBreak.dir/Source/ResourceManager.cpp.o"
+  "../bin/TotalBreak.pdb"
+  "../bin/TotalBreak"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TotalBreak.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
